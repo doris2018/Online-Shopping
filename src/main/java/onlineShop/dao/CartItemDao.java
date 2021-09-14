0cb4f57b81939fea629890cb08c55b39a5707e67
doris2018 <36977334+doris2018@users.noreply.github.com>
2021-09-14 11:12:14 -0700

    Update CartItemDao.java

--- src/main/java/onlineShop/dao/CartItemDao.java
+++ src/main/java/onlineShop/dao/CartItemDao.java
@@ -1,7 +1,6 @@
 package onlineShop.dao;
 
 import java.util.List;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
